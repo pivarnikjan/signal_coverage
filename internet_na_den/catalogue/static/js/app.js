37b3f3c89f7a37f637f81b58ea66726bb0952cdb
Jan Pivarnik <pivarnikjan@gmail.com>
2016-12-16 14:52:29 +0100

    [ADD] cleaned code, better encapsulation

--- internet_na_den/catalogue/static/js/app.js
+++ internet_na_den/catalogue/static/js/app.js
@@ -4,7 +4,7 @@ var access_token = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtO
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + access_token, {
     maxZoom: 20,
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>' + ' | ' + 'Jan Pivarnik',
     id: 'mapbox.streets'
 }).addTo(mymap);
 
@@ -27,16 +27,6 @@ function Coordinates(latitude, longitude) {
     this.lng = longitude;
 }
 
-var bratislava = new Coordinates(48.143889, 17.109722);
-var michalovce = new Coordinates(48.755677, 21.918386);
-var trnava = new Coordinates(48.380416, 17.587819);
-
-var banska_bystrica = new Coordinates(48.73946, 19.15349);
-var levice = new Coordinates(48.216454, 18.600445);
-
-var trencin = new Coordinates(48.891132, 18.042297);
-var piestany = new Coordinates(48.589233, 17.834047);
-
 var convertCoordinates = function (startingPosition, endingPosition) {
     var pointA = new L.LatLng(startingPosition.lat, startingPosition.lng);
     var pointB = new L.LatLng(endingPosition.lat, endingPosition.lng);
@@ -50,6 +40,35 @@ var addLine = function (startingPosition, endingPosition, color) {
         smoothFactor: 1
     });
     return polyLine;
+};
+
+var convertStatusToBool = function (status) {
+    return status === 'UP';
+};
+
+var signalColor = function (status1, status2) {
+    if (convertStatusToBool(status1) && convertStatusToBool(status2)) {
+        return 'blue'
+    }
+    else {
+        return 'red'
+    }
+};
+
+var makeConnection = function (record) {
+    var startingPosition = {
+        lat: record.site_lat,
+        lng: record.site_lng
+    };
+
+    var endingPosition = {
+        lat: record.cpe_lat,
+        lng: record.cpe_lng
+    };
+    addMarker(startingPosition).addTo(mymap);
+    addMarker(endingPosition).addTo(mymap);
+    addLine(startingPosition, endingPosition, signalColor(record.site_status, record.cpe_status)).addTo(mymap);
+
 };
 
 var rad = function (x) {
@@ -67,18 +86,28 @@ getDistance = function (p1, p2) {
     return R * c; // returns the distance in meter
 };
 
-addMarker(michalovce).addTo(mymap);
-addMarker(bratislava).addTo(mymap);
-addMarker(trnava).addTo(mymap);
-// coverage_area(getDistance(bratislava,trnava),bratislava).addTo(mymap);
+var superObject = {
 
-addMarker(banska_bystrica).addTo(mymap);
-addMarker(levice).addTo(mymap);
-// coverage_area(getDistance(banska_bystrica,levice),banska_bystrica).addTo(mymap);
+    site_lat: 48.143889,
+    site_lng: 17.109722,
 
-addMarker(trencin).addTo(mymap);
-addMarker(piestany).addTo(mymap);
-// coverage_area(getDistance(trencin,piestany),trencin).addTo(mymap);
+    cpe_lat: 48.380416,
+    cpe_lng: 17.587819,
 
-addLine(bratislava, trnava, 'red').addTo(mymap);
-addLine(piestany, trencin, 'blue').addTo(mymap);
+    site_status: 'UP',
+    cpe_status: 'UP'
+};
+
+
+var bratislava = new Coordinates(48.143889, 17.109722);
+var michalovce = new Coordinates(48.755677, 21.918386);
+var trnava = new Coordinates(48.380416, 17.587819);
+
+var banska_bystrica = new Coordinates(48.73946, 19.15349);
+var levice = new Coordinates(48.216454, 18.600445);
+
+var trencin = new Coordinates(48.891132, 18.042297);
+var piestany = new Coordinates(48.589233, 17.834047);
+
+
+makeConnection(superObject);
--- internet_na_den/catalogue/views.py
+++ internet_na_den/catalogue/views.py
@@ -1,11 +1,25 @@
+import logging
 from django.shortcuts import render
+
+from .models import SignalCoverage
 from .forms import SignalCoverageForm
 
 
+_logger = logging.getLogger(__name__)
+
+
 def index(request):
-    return render(request, 'index.html')
+    queryset = SignalCoverage.objects.order_by()
+    context = {
+        "object_list": queryset
+    }
+    print queryset
+    # for item in queryset:
+    #     print item
+    return render(request, 'index.html', context)
 
 
 def post_new(request):
     form = SignalCoverageForm()
+
     return render(request, 'post_edit.html', {'form': form})
